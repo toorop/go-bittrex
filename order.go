aadbde30454166a412835661a7108dbe5c7e3cb5
Julian Yap <julianokyap@gmail.com>
2016-03-12 23:39:44 -1000

    Set TimeStamp field to jTime type. Run go fmt

--- order.go
+++ order.go
@@ -3,7 +3,7 @@ package bittrex
 type Order struct {
 	OrderUuid         string  `json:"OrderUuid"`
 	Exchange          string  `json:"Exchange"`
-	TimeStamp         string  `json:"TimeStamp"`
+	TimeStamp         jTime   `json:"TimeStamp"`
 	OrderType         string  `json:"OrderType"`
 	Limit             float64 `json:"Limit"`
 	Quantity          float64 `json:"Quantity"`
@@ -15,28 +15,27 @@ type Order struct {
 
 // For getorder
 type Order2 struct {
-    AccountId         string
-    OrderUuid         string  `json:"OrderUuid"`
-	Exchange          string  `json:"Exchange"`
-	Type              string
-	Quantity          float64 `json:"Quantity"`
-	QuantityRemaining float64 `json:"QuantityRemaining"`
-	Limit             float64 `json:"Limit"`
-    Reserved          float64
-    ReserveRemaining  float64
-    CommissionReserved          float64
-    CommissionReserveRemaining  float64
-    CommissionPaid    float64
-	Price             float64 `json:"Price"`
-	PricePerUnit      float64 `json:"PricePerUnit"`
-	Opened            string
-	Closed            string
-	IsOpen            bool
-	Sentinel          string
-	CancelInitiated   bool
-	ImmediateOrCancel bool
-	IsConditional     bool
-	Condition         string
-	ConditionTarget   string
+	AccountId                  string
+	OrderUuid                  string `json:"OrderUuid"`
+	Exchange                   string `json:"Exchange"`
+	Type                       string
+	Quantity                   float64 `json:"Quantity"`
+	QuantityRemaining          float64 `json:"QuantityRemaining"`
+	Limit                      float64 `json:"Limit"`
+	Reserved                   float64
+	ReserveRemaining           float64
+	CommissionReserved         float64
+	CommissionReserveRemaining float64
+	CommissionPaid             float64
+	Price                      float64 `json:"Price"`
+	PricePerUnit               float64 `json:"PricePerUnit"`
+	Opened                     string
+	Closed                     string
+	IsOpen                     bool
+	Sentinel                   string
+	CancelInitiated            bool
+	ImmediateOrCancel          bool
+	IsConditional              bool
+	Condition                  string
+	ConditionTarget            string
 }
-
