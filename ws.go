89469ccc955a82e692fb26adf542e66fdcbfa654
Aleksandr Demakin <alexander.demakin@gmail.com>
2017-09-06 15:17:34 +0300

    ws: do all ws connect async with timeout due to slow api.

--- ws.go
+++ ws.go
@@ -28,58 +28,89 @@ type ExchangeState struct {
 	Initial    bool
 }
 
-func (b *Bittrex) SubscribeExchangeUpdate(market string, dataCh chan<- ExchangeState, stop <-chan bool) error {
-	client := signalr.NewWebsocketClient()
-	sendUpdate := func(st ExchangeState) {
+func doAsyncTimeout(f func() error, tmFunc func(error), timeout time.Duration) error {
+	errs := make(chan error)
+	go func() {
+		err := f()
 		select {
-		case dataCh <- st:
+		case errs <- err:
 		default:
+			if tmFunc != nil {
+				tmFunc(err)
+			}
 		}
+	}()
+	select {
+	case err := <-errs:
+		return err
+	case <-time.After(timeout):
+		return errors.New("callhub timeout")
 	}
+}
+
+func sendStateAsync(dataCh chan<- ExchangeState, st ExchangeState) {
+	select {
+	case dataCh <- st:
+	default:
+	}
+}
+
+func subForMarket(client *signalr.Client, market string) (json.RawMessage, error) {
+	_, err := client.CallHub(WS_HUB, "SubscribeToExchangeDeltas", market)
+	if err != nil {
+		return json.RawMessage{}, err
+	}
+	return client.CallHub(WS_HUB, "QueryExchangeState", market)
+}
+
+func parseStates(messages []json.RawMessage, dataCh chan<- ExchangeState, market string) {
+	for _, msg := range messages {
+		var st ExchangeState
+		if err := json.Unmarshal(msg, &st); err != nil {
+			continue
+		}
+		if st.MarketName != market {
+			continue
+		}
+		sendStateAsync(dataCh, st)
+	}
+}
+
+func (b *Bittrex) SubscribeExchangeUpdate(market string, dataCh chan<- ExchangeState, stop <-chan bool) error {
+	const timeout = 5 * time.Second
+	client := signalr.NewWebsocketClient()
 	client.OnClientMethod = func(hub string, method string, messages []json.RawMessage) {
 		if hub != "CoreHub" || method != "updateExchangeState" {
 			return
 		}
-		for _, msg := range messages {
-			var st ExchangeState
-			if err := json.Unmarshal(msg, &st); err != nil {
-				continue
-			}
-			if st.MarketName != market {
-				continue
-			}
-			sendUpdate(st)
-		}
+		parseStates(messages, dataCh, market)
 	}
-	if err := client.Connect("https", WS_BASE, []string{WS_HUB}); err != nil {
+	err := doAsyncTimeout(func() error {
+		return client.Connect("https", WS_BASE, []string{WS_HUB})
+	}, func(err error) {
+		if err == nil {
+			client.Close()
+		}
+	}, timeout)
+	if err != nil {
 		return err
 	}
 	defer client.Close()
-	errCh := make(chan error, 1)
 	var msg json.RawMessage
-	go func() {
-		_, err := client.CallHub(WS_HUB, "SubscribeToExchangeDeltas", market)
-		if err != nil {
-			errCh <- err
-			return
-		}
-		msg, err = client.CallHub(WS_HUB, "QueryExchangeState", market)
-		errCh <- err
-	}()
-	select {
-	case err := <-errCh:
-		if err != nil {
-			return err
-		}
-	case <-time.After(time.Second * 5):
-		return errors.New("callhub timeout")
+	err = doAsyncTimeout(func() error {
+		var err error
+		msg, err = subForMarket(client, market)
+		return err
+	}, nil, timeout)
+	if err != nil {
+		return err
 	}
 	var st ExchangeState
-	if err := json.Unmarshal(msg, &st); err != nil {
+	if err = json.Unmarshal(msg, &st); err != nil {
 		return err
 	}
 	st.Initial = true
-	sendUpdate(st)
+	sendStateAsync(dataCh, st)
 	<-stop
 	return nil
 }
