2a3357db30502e0dce689b910667509010168140
Julian Yap <julianokyap@gmail.com>
2018-11-01 01:00:39 -0700

    Add more fields

--- market.go
+++ market.go
@@ -8,7 +8,9 @@ type Market struct {
 	MinTradeSize       float64 `json:"MinTradeSize"`
 	MarketName         string  `json:"MarketName"`
 	IsActive           bool    `json:"IsActive"`
+	IsRestricted       bool    `json:"IsRestricted"`
 	Notice             string  `json:"Notice"`
 	IsSponsored        bool    `json:"IsSponsored"`
 	LogoUrl            string  `json:"LogoUrl"`
+	Created            string  `json:"Created"`
 }
