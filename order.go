b466e78e31b59470c0acaaa1b53c8c2df005c9d1
Boris Yaroslav <sunheretic13@gmail.com>
2019-08-25 00:39:49 +0300

    Order format changed
    - Support for new fields in the Order struct.The "description" was found on the official Bittrex API documentation page and verified on a real request.
    - Added interpretation of date fields

--- order.go
+++ order.go
@@ -3,16 +3,23 @@ package bittrex
 import "github.com/shopspring/decimal"
 
 type Order struct {
+	Uuid              *string
 	OrderUuid         string          `json:"OrderUuid"`
 	Exchange          string          `json:"Exchange"`
-	TimeStamp         jTime           `json:"TimeStamp"`
 	OrderType         string          `json:"OrderType"`
 	Limit             decimal.Decimal `json:"Limit"`
 	Quantity          decimal.Decimal `json:"Quantity"`
 	QuantityRemaining decimal.Decimal `json:"QuantityRemaining"`
-	Commission        decimal.Decimal `json:"Commission"`
 	Price             decimal.Decimal `json:"Price"`
 	PricePerUnit      decimal.Decimal `json:"PricePerUnit"`
+	CommissionPaid    decimal.Decimal
+	Opened            jTime
+	Closed            *jTime
+	CancelInitiated   bool
+	ImmediateOrCancel bool
+	IsConditional     bool
+	Condition         string
+	ConditionTarget   decimal.Decimal
 }
 
 // For getorder
@@ -31,8 +38,8 @@ type Order2 struct {
 	CommissionPaid             decimal.Decimal
 	Price                      decimal.Decimal `json:"Price"`
 	PricePerUnit               decimal.Decimal `json:"PricePerUnit"`
-	Opened                     string
-	Closed                     string
+	Opened                     jTime
+	Closed                     *jTime
 	IsOpen                     bool
 	Sentinel                   string
 	CancelInitiated            bool
