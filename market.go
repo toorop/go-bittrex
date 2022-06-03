0fabd89098ec5f3422398d69ba0aa5b3d5d787d8
Julian Yap <julianokyap@gmail.com>
2022-06-03 11:21:31 -0700

    Add additional fields to MarketV3 and CurrencyV3

--- market.go
+++ market.go
@@ -1,34 +1,36 @@
 package bittrex
 
 import (
-	"github.com/shopspring/decimal"
 	"time"
+
+	"github.com/shopspring/decimal"
 )
 
 type Market struct {
-	MarketCurrency     string  `json:"MarketCurrency"`
-	BaseCurrency       string  `json:"BaseCurrency"`
-	MarketCurrencyLong string  `json:"MarketCurrencyLong"`
-	BaseCurrencyLong   string  `json:"BaseCurrencyLong"`
+	MarketCurrency     string          `json:"MarketCurrency"`
+	BaseCurrency       string          `json:"BaseCurrency"`
+	MarketCurrencyLong string          `json:"MarketCurrencyLong"`
+	BaseCurrencyLong   string          `json:"BaseCurrencyLong"`
 	MinTradeSize       decimal.Decimal `json:"MinTradeSize"`
-	MarketName         string  `json:"MarketName"`
-	IsActive           bool    `json:"IsActive"`
-	IsRestricted       bool    `json:"IsRestricted"`
-	Notice             string  `json:"Notice"`
-	IsSponsored        bool    `json:"IsSponsored"`
-	LogoUrl            string  `json:"LogoUrl"`
-	Created            string  `json:"Created"`
+	MarketName         string          `json:"MarketName"`
+	IsActive           bool            `json:"IsActive"`
+	IsRestricted       bool            `json:"IsRestricted"`
+	Notice             string          `json:"Notice"`
+	IsSponsored        bool            `json:"IsSponsored"`
+	LogoUrl            string          `json:"LogoUrl"`
+	Created            string          `json:"Created"`
 }
 
-
 type MarketV3 struct {
-	Symbol              string   `json:"symbol"`
-	BaseCurrencySymbol  string   `json:"baseCurrencySymbol"`
-	QuoteCurrencySymbol string   `json:"quoteCurrencySymbol"`
-	MinTradeSize        decimal.Decimal   `json:"minTradeSize"`
-	Precision           int32   `json:"precision"`
-	Status              string   `json:"status"`
-	CreatedAt           time.Time   `json:"createdAt"`
-	Notice              string   `json:"notice"`
-	ProhibitedIn        []string `json:"prohibitedIn"`
+	Symbol                   string          `json:"symbol"`
+	BaseCurrencySymbol       string          `json:"baseCurrencySymbol"`
+	QuoteCurrencySymbol      string          `json:"quoteCurrencySymbol"`
+	MinTradeSize             decimal.Decimal `json:"minTradeSize"`
+	Precision                int32           `json:"precision"`
+	Status                   string          `json:"status"`
+	CreatedAt                time.Time       `json:"createdAt"`
+	Notice                   string          `json:"notice"`
+	ProhibitedIn             []string        `json:"prohibitedIn"`
+	AssociatedTermsOfService []string        `json:"associatedTermsOfService"`
+	Tags                     []string        `json:"tags"`
 }
